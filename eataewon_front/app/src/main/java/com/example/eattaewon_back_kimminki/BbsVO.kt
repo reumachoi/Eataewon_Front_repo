465bd28985e171d63f4a82233951e614c6cbe3aa
MK <erenad0105@gmail.com>
2022-03-25 14:15:45 +0900

    테스트 푸쉬

---
+++ eataewon_front/app/src/main/java/com/example/eattaewon_back_kimminki/BbsVO.kt
@@ -0,0 +1,5 @@
+package com.example.eattaewon_back_kimminki
+
+class BbsVO(var id: String, var seq: Int?, var title: String, var content: String, var picture: Int,
+            var hashtag: String, var wdate: String, var shopname: String, var address: String,
+            var latitude: Double, var longtitude: Double, var readcnt: Int, var likecnt: Int)
---
+++ eataewon_front/app/src/main/java/com/example/eattaewon_back_kimminki/MypageFragment.kt
@@ -0,0 +1,6 @@
+package com.example.eattaewon_back_kimminki
+
+import androidx.fragment.app.Fragment
+
+class MypageFragment: Fragment(R.layout.fragment_mypage) {
+}
