298405e37f59289ceffa04e111116c9c76769d48
areum <cunecoder9@gmail.com>
2022-03-28 17:34:05 +0900

    Update MainActivity.kt

--- eataewon_front/app/src/main/java/com/example/eattaewon/MainActivity.kt
+++ eataewon_front/app/src/main/java/com/example/eattaewon/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.eattaewon;
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.eattaewon.connect.MemberDto
-import com.example.eattaewon.connect.RetrofitClient
-
 import com.example.eattaewon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(),View.OnClickListener{
