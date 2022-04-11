437b29963dc2811fdd02c12641591051ade8a4a6
DESKTOP-PTQKL3C\Administrator <an881201@naver.com>
2022-04-11 15:01:14 +0900

    아이디 찾기 레이아웃 ui 변경 및 관련 라디오 버튼 스타일 2개 추가 및 Activity파일 수정

--- eataewon_front/app/src/main/java/com/example/eataewon/FindUserIdActivity.kt
+++ eataewon_front/app/src/main/java/com/example/eataewon/FindUserIdActivity.kt
@@ -3,13 +3,11 @@ package com.example.eataewon
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
-import androidx.core.view.isVisible
-import androidx.core.widget.doAfterTextChanged
 import com.example.eataewon.connect.MemberDao
 import com.example.eataewon.connect.MemberDto
 import com.example.eataewon.databinding.ActivityFindUserIdBinding
@@ -23,16 +21,45 @@ class FindUserIdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+        binding.findRadioBtn.setOnCheckedChangeListener { _, checkedId ->
+
+            when(checkedId){
+
+                R.id.find_radioBtn1->{
+                    Toast.makeText(this,"1번",Toast.LENGTH_SHORT).show()
+                    find_id_layout.isVisible = true
+                    find_email_layout.isInvisible = true
+                }
+
+                R.id.find_radioBtn2->{
+                    Toast.makeText(this,"2번",Toast.LENGTH_SHORT).show()
+                    find_id_layout.isInvisible = true
+                    find_email_layout.isVisible = true
+                }
+
+            }
+        }
         binding.findIdBtn.setOnClickListener{
-            val id = findIdText.text.toString().trim()
+            val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+            val id = binding.findIdText.text.toString().trim()
             println(id)
             val idcheck = MemberDao.getInstance().getId(id)
 
+
+            //안도현 변경 레이아웃으로 합병
             if(idcheck.equals("NO")){
                 binding.findResultText.text = "입력하신 정보와 일치하는 계정을 찾았습니다 \n 비밀번호를 재설정해주세요"
                 binding.findResultText.isVisible = true
                 binding.findResultText.setTextColor(Color.BLUE)
+
+                //추가
+                find_pw_layout.isVisible = true
+
                 binding.findIdResultId.text = id
+
             }else{
                 binding.findResultText.text = "입력하신 정보와 일치하는 계정을 찾지못했습니다"
                 binding.findResultText.isVisible = true
@@ -40,16 +67,24 @@ class FindUserIdActivity : AppCompatActivity() {
             }
         }
 
+        //안도현 변경 레이아웃으로 합병
         binding.findEamilBtn.setOnClickListener{
-            val email = findEmailText.text.toString().trim()
+            val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            manager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
 
+            val email = findEmailText.text.toString().trim()
             val findId = MemberDao.getInstance().getEmail(email)
 
             if(findId!=""){
                 binding.findResultText.text = "입력하신 정보와 일치하는 계정을 찾았습니다 \n 비밀번호를 재설정해주세요"
                 binding.findResultText.isVisible = true
                 binding.findResultText.setTextColor(Color.BLUE)
+
+                //추가
+                find_pw_layout.isVisible = true
+
                 binding.findIdResultId.text = findId
+
             }else{
                 binding.findResultText.text = "입력하신 정보와 일치하는 계정을 찾지못했습니다"
                 binding.findResultText.isVisible = true
