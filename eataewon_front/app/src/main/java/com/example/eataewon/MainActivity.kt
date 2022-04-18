d6db941efc47551b6b70b73e9dc2531d5ef4de90
areum <cunecoder9@gmail.com>
2022-04-18 11:28:42 +0900

    0418 11:28

--- eataewon_front/app/src/main/java/com/example/eataewon/MainActivity.kt
+++ eataewon_front/app/src/main/java/com/example/eataewon/MainActivity.kt
@@ -81,10 +81,10 @@ class MainActivity : AppCompatActivity(),View.OnClickListener{
         // 카카오 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
-                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
             }
             else if (tokenInfo != null) {
-                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, SecondActivity::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                 finish()
